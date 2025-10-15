c568d047b87494352ea2078e6f972a339451f56d
shaza <shazawagdy@gmail.com>
2025-10-16 01:53:22 +0300

    init backend

---
+++ backend/app/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SYNC_SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+ASYNC_SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
+async_session = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    class_=AsyncSession,
+    bind=engine,
+    expire_on_commit=False,
+)
+
+Base = declarative_base()
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
