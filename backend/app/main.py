25c5ba38e83dda05ac7ef7828143d675dd9a9f7e
shaza <shazawagdy@gmail.com>
2025-10-16 01:53:22 +0300

    init backend

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.params import Depends
+
+from app.database import get_session
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my FastAPI backend!"}
+
+
+@app.get("/health")
+async def health_check(session: AsyncSession = Depends(get_session)):
+    table = await session.scalars(text("SELECT 1"))
+    return {"status": "ok", "db_response": table.all()}
