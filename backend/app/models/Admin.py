1852377efe4659d8071327e842cf586c05c02400
Shaza Ahmed <shazawagdy@gmail.com>
2026-04-24 17:16:59 +0300

    Auth APIs and minor schema changes

--- backend/app/models/Admin.py
+++ backend/app/models/Admin.py
@@ -1,11 +1,10 @@
 
-from app.models import User
+from app.database import Base
 from sqlalchemy import Column, Integer, ForeignKey
 
-class Admin(User):
+class Admin(Base):
     __tablename__ = "administrators"
 
-    adminID = Column("adminID", Integer, primary_key=True)
+    adminID = Column("adminID", Integer, primary_key=True, index=True)
     userID  = Column("userID", Integer, ForeignKey("users.userID"), nullable=False)
 
-    __mapper_args__ = {"polymorphic_identity": "admin"}
---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,15 @@
+# app/schemas/__init__.py
+
+from app.schemas.SignUpRequest import SignUpRequest
+from app.schemas.SignUpResponse import SignUpResponse
+from app.schemas.SignInRequest import SignInRequest
+from app.schemas.SignInResponse import SignInResponse
+
+# app/schemas/__init__.py
+
+from app.schemas import (
+    SignUpRequest,
+    SignUpResponse,
+    SignInRequest,
+    SignInResponse
+)
