5346b385e3e9e080858d5a43ccc6183c01daf7d8
Rania_Raafat <mohrealrafet@gmail.com>
2026-05-07 22:18:02 +0300

    coach dashboard

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\downloads\\flutter_windows_3.41.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\downloads\\flutter_windows_3.41.5-stable\\flutter"
+  "PROJECT_DIR=E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend"
+  "FLUTTER_ROOT=E:\\downloads\\flutter_windows_3.41.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend"
+  "FLUTTER_TARGET=E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MmM5ZWIyMDczOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MDUyZjMxZDExNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\FCAI\\Senior_Year\\Titan_Gym-Mangement-Application\\frontend\\.dart_tool\\package_config.json"
+)
