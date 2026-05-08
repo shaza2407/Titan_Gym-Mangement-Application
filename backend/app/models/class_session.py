7f85a20fb873e4c04520c4a3ea7895cd4148dcb0
Rania_Raafat <mohrealrafet@gmail.com>
2026-05-08 13:31:26 +0300

    Add request-new-class endpoint and edit request-new-class screen

--- backend/app/models/class_session.py
+++ backend/app/models/class_session.py
@@ -1,13 +1,5 @@
-from sqlalchemy import Column,Integer,String,Date,Time,ForeignKey, Enum as SQLEnum
+from sqlalchemy import Column,Integer,String,Date,Time,ForeignKey
 from app.database import Base
-from sqlalchemy.orm import relationship
-
-import enum
-
-class RequestStatus(str,enum.Enum):
-    PENDING = "pending"
-    APPROVED = "approved"
-    REJECTED = "rejected"
 
 
 class ClassSession(Base):
@@ -22,19 +14,6 @@ class ClassSession(Base):
     # gym_id = Column(Integer,ForeignKey("gyms.id"),nulable=False)
     coach_id = Column(Integer, ForeignKey("coaches.coachID"), nullable=False)
 
-    current_students = Column(Integer,default=0)
-    max_students = Column(Integer, nullable=False)
+    current_clients = Column(Integer,default=0)
+    max_clients = Column(Integer, nullable=False)
 
-
-class ClassRequest(Base):
-    __tablename__ = "class_requests"
-
-    id = Column(Integer, primary_key=True, index=True)
-    class_type = Column(String, nullable=False)
-    requested_date = Column(Date,nullable=False)
-    requested_time = Column(Time,nullable=False)
-
-    status = Column(SQLEnum(RequestStatus), default=RequestStatus.PENDING)
-
-    # gym_id = Column(Integer, ForeignKey("gyms.id"), nullable=False)
-    coach_id = Column(Integer, ForeignKey("coaches.coachID"), nullable=False)
