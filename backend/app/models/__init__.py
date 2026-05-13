7411d530803b3f09669c8027eed5488d196bfd44
lamoro8800 <lamoro820@gmail.com>
2026-05-14 00:45:20 +0300

    Testing Version

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,12 +4,15 @@ from app.models.client import Client
 from app.models.coach import Coach
 from app.models.Admin import Admin
 from app.models.Gym import Gym
+from app.models.gym_clients_membership import GymClientMembership
+from app.models.gym_coachs_membership import GymCoachMembership
+from app.models.member_invitation import MemberInvitation
+
 from app.models.Machine import Machine
 from app.models.gymMachineInventory import GymMachineInventory
 from app.models.class_request import ClassRequest
 from app.models.class_session import ClassSession
-from app.models.member_invitation import MemberInvitation, InvitationStatus
-from app.models.gym_clients_membership import GymClientMembership, ClientMembershipStatus
 
 
-__all__ = [User, Client, Coach, Admin,ClassSession,ClassRequest ,Gym, Machine, GymMachineInventory, MemberInvitation, InvitationStatus, GymClientMembership, ClientMembershipStatus]  
+__all__ = [User, Client, Coach, Admin,ClassSession,ClassRequest ,Gym, Machine, GymMachineInventory,
+           GymClientMembership, GymCoachMembership, MemberInvitation]
