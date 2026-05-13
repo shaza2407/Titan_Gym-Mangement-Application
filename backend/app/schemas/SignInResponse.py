0dc7f4671d0e1c4c7d2f81820a5335cc663ff8c1
aishaashour <aishaashour2004@gmail.com>
2026-05-13 22:51:20 +0300

    just correcting thing to work on separate branch

--- backend/app/schemas/SignInResponse.py
+++ backend/app/schemas/SignInResponse.py
@@ -8,9 +8,3 @@ class SignInResponse(BaseModel):
     token_type: str = "bearer"       # JWT token
     role: UserRole
     userID: int
-
-    # fields Flutter needs for routing 
-    is_gym_connected: bool          = False
-    gymID:            Optional[int] = None
-    gymName:          Optional[str] = None
-    clientID:         Optional[int] = None
