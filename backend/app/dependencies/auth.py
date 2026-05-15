b9012e502a53ee70fa48b1a92d197204656b35f2
Abdallahyones <abdallhmo828@gmail.com>
2026-05-15 16:00:47 +0300

    modifie dependencies/auth.py

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,29 +1,25 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from jose import jwt, JWTError
-
 from app.database import get_session
 from app.models import User
 from app.models.Admin import Admin
 
 SECRET_KEY = "your-secret-key"
-ALGORITHM  = "HS256"
+ALGORITHM = "HS256"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signin")
 
 
-#decode token & return current user
-def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    db: Session = Depends(get_session)
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_session)
 ) -> User:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        userID  = int(payload.get("sub"))
+        userID = int(payload.get("sub"))
     except (JWTError, TypeError):
         raise HTTPException(401, "Invalid or expired token")
 
@@ -31,13 +27,15 @@ def get_current_user(
     user = result.scalars().first()
     if not user:
         raise HTTPException(401, "User not found")
-
     return user
 
 
-# to use in routes that require specific roles
-def require_admin(current_user: User = Depends(get_current_user)):
-    if current_user.role != "admin":
+async def require_admin(token: str = Depends(oauth2_scheme),
+                        db: AsyncSession = Depends(get_session)
+                        ) -> Admin:
+    # Step 1: verify token & get user
+    user = await get_current_user(token=token, db=db)
+    if user.role != "admin":
         raise HTTPException(403, "Admins only")
 
     result = await db.execute(select(Admin).filter(Admin.userID == user.userID))
@@ -47,10 +45,12 @@ def require_admin(current_user: User = Depends(get_current_user)):
 
     return admin
 
+
 async def require_coach(current_user: User = Depends(get_current_user)):
     if current_user.role != "coach":
         raise HTTPException(403, "Coaches only")
     return current_user
+
 
 async def require_client(current_user: User = Depends(get_current_user)):
     if current_user.role != "client":
