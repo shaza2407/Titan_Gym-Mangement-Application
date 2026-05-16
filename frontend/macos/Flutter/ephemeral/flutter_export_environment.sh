925108294575f9ea588ecd1f1e8bac9f7fda0d25
Shaza Ahmed <shazawagdy@gmail.com>
2026-05-16 22:01:17 +0300

    database migratioin

--- frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\downloads\flutter_windows_3.41.5-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\FCAI\Senior_Year\Titan_Gym-Mangement-Application\frontend"
+export "FLUTTER_ROOT=/Users/applecare/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/applecare/Desktop/Titan_Gym-Mangement-Application/frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
