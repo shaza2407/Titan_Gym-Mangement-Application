24e2c690904e63a254098504cbebb5eeb57145e1
Shaza Ahmed <shazawagdy@gmail.com>
2026-06-20 02:35:41 +0300

    announcement types

--- backend/app/schemas/announcement_schema.py
+++ backend/app/schemas/announcement_schema.py
@@ -1,15 +1,23 @@
+# announcement_schema.py
 from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Literal
 
 class CreateAnnouncementRequest(BaseModel):
     title: str = Field(..., max_length=255)
     content: str = Field(..., max_length=1000)
+    reciever: Literal[
+        'Clients only',
+        'Coaches only',
+        'Clients and Coaches',
+    ] = 'Clients and Coaches'  
 
 class AnnouncementResponse(BaseModel):
     announce_id: int
     gymID: int
     title: str
     content: str
+    reciever: str           
     created_at: datetime
 
     class Config:
